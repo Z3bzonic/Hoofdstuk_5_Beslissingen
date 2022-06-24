ed8b4a99afe0ee62e8e4bc11541e5474c7d6962b
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-24 11:07:54 +0200

    Add files via upload

---
+++ Schoenverkoper/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ++ declaratie +++
+
+
+int kortingDenominator;
+int aantal;
+int totaal;
+
+// ++ invoer +++
+
+Console.WriteLine("Geef het aantal in om korting te bekomen");
+kortingDenominator = int.Parse((Console.ReadLine()));
+Console.WriteLine($"De gekozen korting is {kortingDenominator}");
+
+Console.WriteLine("Geef het aantal schoenparen in");
+aantal = int.Parse((Console.ReadLine()));
+
+// ++ berekening van bedrag +++
+
+if (aantal > kortingDenominator)
+{
+    totaal = kortingDenominator*20;
+    totaal = totaal + ((aantal - kortingDenominator)*10);
+}
+else
+{
+    totaal = aantal * 20;
+}
+
+Console.WriteLine(totaal);
