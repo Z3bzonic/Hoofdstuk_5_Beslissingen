7378501026ae0912ea234fd262962fdc17e8c932
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-24 14:45:45 +0200

    Add files via upload

---
+++ Casino/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random random = new Random();
+
+Console.WriteLine("Raad de computerworp van 1 tot 6");
+int gooi = random.Next(1,6);
+
+int raad = int.Parse(Console.ReadLine());
+
+if (gooi == raad)
+{
+    Console.WriteLine("Je hebt gewonnen, proficiat!!");
+}
+else
+{
+    Console.WriteLine("Je hebt verloren, ook proficiat!!");
+}
