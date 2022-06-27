c7c4c68c00957164af52d7ff1e5bf18a38a25574
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-27 08:38:22 +0200

    Add files via upload

---
+++ Schrikkeljaar/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Voer een jaartal in");
+int jaar = int.Parse(Console.ReadLine());
+
+if ((jaar % 4 == 0) && ((jaar % 100 != 0) || (jaar % 400 == 0)))
+{
+    Console.WriteLine($"Jaar {jaar} is een schrikkeljaar");
+}
+else
+{
+    Console.WriteLine($"Jaar {jaar} is geen schrikkeljaar");
+}
