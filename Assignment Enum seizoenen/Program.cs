8d4ca02e79ef9615cf9222db665d8fa664b25bd0
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-27 09:53:21 +0200

    Add files via upload

---
+++ Assignment Enum seizoenen/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment_Enum_seizoenen;
+
+Seizoenen keuze = Seizoenen.Default;
+int maand = 13;
+
+while (maand != 0)
+{
+    Console.WriteLine("Geef de maandnummer in");
+    maand = int.Parse(Console.ReadLine());
+
+switch (maand)
+{
+    case 1:
+    case 2:
+    case 3:
+        keuze = (Seizoenen)Seizoenen.Winter ;
+        break;
+    case 4:
+    case 5:
+    case 6:
+        keuze = Seizoenen.Lente;
+        break;
+    case 7:
+    case 8:                                       
+    case 9:
+        break;
+        keuze = Seizoenen.Zomer;
+    case 10:
+    case 11:
+    case 12:
+        keuze = (Seizoenen)5;
+        break;
+    default:
+        break;
+}
+if ((keuze == Seizoenen.Winter) || (keuze == Seizoenen.Herfst))
+{
+    Console.WriteLine("Het is een koud seizoen");
+}
+if ((keuze == Seizoenen.Zomer) || (keuze == Seizoenen.Lente))
+{
+    Console.WriteLine("Het is een warm seizoen");
+}
+
+}
---
+++ Assignment Enum seizoenen/Seizoenen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Enum_seizoenen
+{
+    internal enum Seizoenen
+    {
+        // default op 0, geen invloed op de rest van de benoemde data tenzij je (Seizoen)0 had gebruikt. Dan is er nog steeds slechts één waarde te wijzigen.
+        Default,
+        Winter,
+        Lente,
+        Zomer,
+        Herfst = 5
+    }
+}
